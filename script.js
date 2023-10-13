0cb99d6fee439353af307a6b300b441b983601cc
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-10-14 02:15:43 +0530

    Add screenController and Refactor the code

--- script.js
+++ script.js
@@ -1,115 +1,204 @@
-const gameBoard = (function(){
-    const row = 3;
-    const col = 3;
-    const board = [];
+const gameBoard = (function () {
+  const row = 3;
+  const col = 3;
+  const board = [];
 
-    // create an array (3 x 3)
+  // create an array (3 x 3)
+  const initBoard = () => {
     for (let i = 0; i < row; i++) {
-        board[i] = [];
-        for(let j = 0 ; j< col; j++){
-            board[i].push('')
-        }        
+      board[i] = [];
+      for (let j = 0; j < col; j++) {
+        board[i].push("");
+      }
     }
+  };
 
-    // change value of cell
-    const changeValue = (row,col,marker)=>{
-        board[row][col] = marker;
-    }
+  //initialize board
+  initBoard();
 
-    // return the board
-    const getBoard = ()=>board;
+  // change value of cell
+  const changeValue = (row, col, marker) => {
+    board[row][col] = marker;
+  };
 
-    return{
-        getBoard,
-        changeValue,
-    }
+  // return the board
+  const getBoard = () => board;
 
+  return {
+    initBoard,
+    getBoard,
+    changeValue,
+  };
 })();
 
-
-function Player(name,marker){
-    return {
-        name,
-        marker,
-    };
+function Player(name, marker) {
+  return {
+    name,
+    marker,
+  };
 }
 
-const gameController = (function(){
-    // playerOne , playerTwo
-    // setPlayers()
-    // playRound()
-    // changeActivePlayer()
-    // checkWinner()
-    // activePlayer
-    const board = gameBoard;
-    
-    let playerOne = "me";
-    let playerTwo = "other";
-    let activePlayer = playerOne;
+const gameController = (function () {
+  const board = gameBoard;
+  const playerOne = Player("playerOne", "X");
+  const playerTwo = Player("playerTwo", "O");
+  let roundCount = 0;
 
-    const setPlayers = (playerOne,playerTwo)=>{
-        this.playerOne =  playerOne;
-        this.playerTwo = playerTwo;
+  let activePlayer = playerOne;
+
+  const setRoundCount = (count)=>{
+    roundCount = count;
+  }
+
+  const changeActivePlayer = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const playRound = ([row, col]) => {
+
+    // play one move
+    if (board.getBoard()[row][col] === "") {
+      roundCount++;
+      board.changeValue(row, col, activePlayer.marker);
+    } else {
+      return;
     }
 
-    const changeActivePlayer = ()=>{
-        activePlayer = activePlayer === playerOne ? playerTwo:playerOne;
+    //check if there is a win
+    if (checkWinner()) {
+      return {
+        finish: true,
+        activePlayer,
+      };
+    } else {
+      changeActivePlayer();
+      return {
+        finish: false,
+        roundCount,
+      };
     }
-    
-    const playRound = ([row,col],marker)=>{
-        // play one move
-       board.changeValue(row,col,activePlayer.marker||marker);
-       //check if there is a win
-       
-       if(checkWinner()){
-        return {
-            finish: true,
-            activePlayer,
-        }
-       }else{
-        changeActivePlayer();
-        return{
-            finish:false,
-        }
-       }
+  };
+
+  const checkWinner = () => {
+    let boardMatrix = board.getBoard();
+
+    if (
+      // row
+      (boardMatrix[0][0] === boardMatrix[0][1] &&
+        boardMatrix[0][0] === boardMatrix[0][2] &&
+        boardMatrix[0][0] !== "") ||
+      (boardMatrix[1][0] === boardMatrix[1][1] &&
+        boardMatrix[1][0] === boardMatrix[1][2] &&
+        boardMatrix[1][0] !== "") ||
+      (boardMatrix[2][0] === boardMatrix[2][1] &&
+        boardMatrix[2][0] === boardMatrix[2][2] &&
+        boardMatrix[2][0] !== "") ||
+      //col
+      (boardMatrix[0][0] === boardMatrix[1][0] &&
+        boardMatrix[0][0] === boardMatrix[2][0] &&
+        boardMatrix[0][0] !== "") ||
+      (boardMatrix[0][1] === boardMatrix[1][1] &&
+        boardMatrix[0][1] === boardMatrix[2][1] &&
+        boardMatrix[0][1] !== "") ||
+      (boardMatrix[0][2] === boardMatrix[1][2] &&
+        boardMatrix[0][2] === boardMatrix[2][2] &&
+        boardMatrix[0][2] !== "") ||
+      //diagonal
+      (boardMatrix[0][0] === boardMatrix[1][1] &&
+        boardMatrix[0][0] === boardMatrix[2][2] &&
+        boardMatrix[0][0] !== "") ||
+      (boardMatrix[0][2] === boardMatrix[1][1] &&
+        boardMatrix[0][2] === boardMatrix[2][0] &&
+        boardMatrix[0][2] !== "")
+    ) {
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    const checkWinner = ()=>{
-        let boardMatrix = board.getBoard();
-        console.log(boardMatrix);
-        if( // row
-            ((boardMatrix[0][0] === boardMatrix[0][1])&&(boardMatrix[0][0] === boardMatrix[0][2])&&(boardMatrix[0][0])!=='')||
-            ((boardMatrix[1][0] === boardMatrix[1][1])&&(boardMatrix[1][0] === boardMatrix[1][2])&&(boardMatrix[1][0]!==''))||
-            ((boardMatrix[2][0] === boardMatrix[2][1])&&(boardMatrix[2][0] === boardMatrix[2][2])&&(boardMatrix[2][0]!==''))||
-            //col
-            ((boardMatrix[0][0] === boardMatrix[1][0])&&(boardMatrix[0][0] === boardMatrix[2][0])&&(boardMatrix[0][0]!==''))||
-            ((boardMatrix[0][1] === boardMatrix[1][1])&&(boardMatrix[0][1] === boardMatrix[2][1])&&(boardMatrix[0][1]!==''))||
-            ((boardMatrix[0][2] === boardMatrix[1][2])&&(boardMatrix[0][2] === boardMatrix[2][2])&&(boardMatrix[0][2]!==''))||
-            //diagonal
-           ( (boardMatrix[0][0] === boardMatrix[1][1])&&(boardMatrix[0][0] === boardMatrix[2][2])&&(boardMatrix[0][0]!== ''))||
-            ((boardMatrix[0][2] === boardMatrix[1][1])&&(boardMatrix[0][2] === boardMatrix[2][0])&&(boardMatrix[0][2] !== ''))
-        ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    return{
-        setPlayers,
-        playRound,
-    }
-
-
+  return {
+    setRoundCount,
+    playRound,
+  };
 })();
-// const screenController = (function(){
-//     const board = gameBoard.getBoard();
-//     board.forEach((row,rowIndex)=>{
-//         row.forEach((col,colIndex)=>{
-//             // console.log(rowIndex+","+colIndex);
-//         })
-//     })
 
+const screenController = (function () {
+  const game = gameController;
+  const board = gameBoard.getBoard();
 
-// })();
+  const boardDiv = document.querySelector(".board");
+  const resultDiv = document.querySelector(".result");
+  const playAgainBtn = document.querySelector("#reset");
+  const nameInputs = document.querySelectorAll("div.name input");
+  let playerOne;
+  let playerTwo;
 
+  // get playerOne and playerTwo
+  nameInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      if (input.id == "playerOne") {
+        playerOne = input.value;
+      } else {
+        playerTwo = input.value;
+      }
+    });
+  });
+
+  // store the result of each move
+  let result = [];
+
+  // update screen with board array data
+  const updateScreen = () => {
+    // make board empty initially
+    boardDiv.textContent = "";
+
+    // render the board
+    board.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        const cell = document.createElement("div");
+        cell.classList = "cell";
+        cell.dataset.row = rowIndex;
+        cell.dataset.col = colIndex;
+        cell.textContent = col;
+        boardDiv.appendChild(cell);
+      });
+    });
+
+    // declare the winner
+    if (result.finish == true) {
+      const winner =
+        result.activePlayer.name == "playerOne" ? playerOne||"Player One" : playerTwo||"Player Two";
+      resultDiv.textContent = `Winner is ${winner} !!`;
+      boardDiv.removeEventListener("click", clickHandlerBoard);
+    }else{
+        if(result.roundCount === 9){
+            resultDiv.textContent = 'Draw !!';
+        }
+    }
+  };
+
+  // perform move on clicking the cell
+  const clickHandlerBoard = (e) => {
+    // get row and col of clicked cell
+    const row = e.target.dataset.row;
+    const col = e.target.dataset.col;
+
+    result = game.playRound([row, col]);
+
+    updateScreen();
+  };
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // reset the game
+  playAgainBtn.addEventListener("click", () => {
+    gameBoard.initBoard();
+    updateScreen();
+    resultDiv.textContent = "";
+    boardDiv.addEventListener("click", clickHandlerBoard);
+    game.setRoundCount(0);
+  });
+
+  // initial render
+  updateScreen();
+})();
